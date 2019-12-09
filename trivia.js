d5ec59681aecabd2d99c6053835d45fed50048cb
KASSIDY_SHADE <kassie2129@gmail.com>
2019-12-09 15:28:24 -0600

    started on functions

--- trivia.js
+++ trivia.js
@@ -1,3 +1,18 @@
 const url = "https://opentdb.com/api.php?amount=50&category=10&type=multiple"
+const question = document.querySelector('.question')
+const optionA = document.querySelector('#optionA')
+const optionB = document.querySelector('#optionB')
+const optionC = document.querySelector('#optionC')
+const optionD = document.querySelector('#optionD')
 
-console.log(url)
+
+fetch(url)
+.then(res => res.json())
+.then(data => newQuestion(data))
+.catch(err => console.log('this is err', err))
+
+function newQuestion(e){
+   let q = data
+    console.log(q)
+    // question.innerText
+}
