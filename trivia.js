bdcde09331c363ecf451f8c913de7bc8774ebb10
KASSIDY_SHADE <kassie2129@gmail.com>
2019-12-10 13:09:08 -0600

    worked through some psuedocode with Kenny deleted useless code

--- trivia.js
+++ trivia.js
@@ -7,49 +7,70 @@ const optionD = document.querySelector('#optionD')
 const next = document.querySelector('.next')
 let clickCount = (0)
 
-fetch(url)
-.then(res => res.json())
-.then(data => setUp(data))
-.catch(err => console.log('this is err', err))
 
-function setUp(data){
-    clickCount = clickCount +1
-    newQuestion(data)
-    answers(data)
-}
-
-function newQuestion(data){
-    for (i=0; i<clickCount; i=i+clickCount){
-    let searchedQuest = data.results[i].question
-    //find out if something needs a quote
-    if (searchedQuest.search("&quot;") == -1 && searchedQuest.search("&#039;") == -1){
-       question.innerText = searchedQuest
-       
-}
-    else if (searchedQuest.search("&quot;") == -1){
-        let questionQuote = searchedQuest.replace(/&#039;/gi,'\"')
-        question.innerText = questionQuote
-}
-    else if(searchedQuest.search("&#039;") == -1){
-        let quotedQuestion = searchedQuest.replace(/&quot;/gi,'\'')
-        question.innerText = quotedQuestion
-    }
-}
-}
 //find correct answer
 //make array for all answers
 //shuffle answers
 // assign all the answers to different buttons
-function answers(data){
-    for (i=0; i<clickCount; i=i+clickCount){
-        //code for the right answer
-    let answer = data.results[i].correct_answer
-    let allAnswers = data.results[i].incorrect_answers
-    console.log(allAnswers)
-}}
 
-next.addEventListener('click',setUp)
 
-// function setUp1(e){
-//     console.log('hi')
-// }
+
+// have the fetch be it's own function => store data into a variable
+// fetch(url)
+// .then(res => res.json())
+// .then(data => newQuestion(data)) // this is an array of data
+// .catch(err => console.log('this is err', err))
+    
+next.addEventListener('click',newQuestion)
+// randomize that array
+    // call the createQuestion() with the first question
+    // call createAnswers( with the first question/answer obj?)
+
+
+function newQuestion (data){
+
+    let searchedQuest = data.results[clickCount].question
+    console.log(searchedQuest)
+    clickCount = clickCount+1
+    next.addEventListener('click',newQuestion)
+
+    //find out if something needs a quote
+    if (searchedQuest.search("&quot;") == -1 && searchedQuest.search("&#039;") == -1){
+       question.innerText = searchedQuest
+       
+    }
+    else if (searchedQuest.search("&quot;") == -1){
+        let questionQuote = searchedQuest.replace(/&#039;/gi,'\"') // source this thing you found
+        question.innerText = questionQuote
+    }
+    else if(searchedQuest.search("&#039;") == -1){
+        let quotedQuestion = searchedQuest.replace(/&quot;/gi,'\'')
+        question.innerText = quotedQuestion
+    }
+    // 
+
+}
+
+// makeAnswers()
+    // parse the data
+    // transplate answer/shuffle logic here
+    // append to DOM
+
+// checkAnswers()
+    // if clicked answer is wrong
+        // dont add a point => maybe add modal
+        // make red border
+        // or make invisible p tag that appears with write answer past into it
+        // document.createElement(p).innerHTML(`data.rightanswer`)
+    // if clicked answer is right
+        // add a point => say congrats!
+
+// timerFunc()
+    // setTimeout(() => {
+        // forloop subtracts 1 from 30
+        // .innerHTML = i
+    // }, 1000)
+
+// easter egg?
+    // bonus points!
+
