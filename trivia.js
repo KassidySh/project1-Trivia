6c712951a77b16f377eb07a563052b597c99e142
KASSIDY_SHADE <kassie2129@gmail.com>
2019-12-10 16:43:20 -0600

    made answers show up in console & questions are shuffled and showing up in h1

--- trivia.js
+++ trivia.js
@@ -6,74 +6,103 @@ const optionC = document.querySelector('#optionC')
 const optionD = document.querySelector('#optionD')
 const next = document.querySelector('.next')
 let clickCount = (0)
-
+let shuffledQs = []
+// next.addEventListener('click',newQuestion)
+next.addEventListener('click',getQuestionList)
 // have the fetch be it's own function => store data into a variable
     
 function getQuestionList () {
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => questionSetUp(data))
     .catch(err => console.log('this is err', err))
 }
+//how I made a shuffle function
+//https://teamtreehouse.com/community/return-mathrandom05
+//https://news.ycombinator.com/item?id=2728914
 
-next.addEventListener('click',getQuestionList)
+function shuffle(firstResults) {
+    for (let i =0; i<firstResults.length; i=i+1) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [firstResults[i], firstResults[j]] = [firstResults[j], firstResults[i]];
 
-console.log(getQuestionList())
+    }
+}
 
+function questionSetUp(data){
+    let firstResults = data.results
+    shuffle(firstResults)
+    shuffledQs = firstResults
+    
+    newQuestion(shuffledQs)
+    createAnswers(shuffledQs)
+}
 // randomize that array
     // call the createQuestion() with the first question
     // call createAnswers( with the first question/answer obj?)
 
 
-// function newQuestion (data){
+function newQuestion (shuffledQs){
 
-//     let searchedQuest = data.results[clickCount].question
-//     console.log(searchedQuest)
-//     clickCount = clickCount+1
-//     next.addEventListener('click',newQuestion)
-
-//     //find out if something needs a quote
-//     if (searchedQuest.search("&quot;") == -1 && searchedQuest.search("&#039;") == -1){
-//        question.innerText = searchedQuest
+    let currentQuestion = shuffledQs[3].question
+    //find out if something needs a quote
+    if (currentQuestion.search("&quot;") == -1 && currentQuestion.search("&#039;") == -1){
+       question.innerText = currentQuestion
        
-//     }
-//     else if (searchedQuest.search("&quot;") == -1){
-//         let questionQuote = searchedQuest.replace(/&#039;/gi,'\"') // source this thing you found
-//         question.innerText = questionQuote
-//     }
-//     else if(searchedQuest.search("&#039;") == -1){
-//         let quotedQuestion = searchedQuest.replace(/&quot;/gi,'\'')
-//         question.innerText = quotedQuestion
-//     }
-//     // 
+    }
+    else if (currentQuestion.search("&quot;") == -1){
+        let questionQuote = currentQuestion.replace(/&#039;/gi,'\"') // source this thing you found
+        question.innerText = questionQuote
+    }
+    else if(currentQuestion.search("&#039;") == -1){
+        let quotedQuestion = currentQuestion.replace(/&quot;/gi,'\'')
+        question.innerText = quotedQuestion
+    }
+}
 
-// }
-
-//find correct answer
-//make array for all answers
-//shuffle answers
+// find correct answer
+// make array for all answers
 // assign all the answers to different buttons
+function createAnswers(shuffledQs){
+    let rightAnswer = shuffledQs[3].correct_answer
+    let answers = shuffledQs[3].incorrect_answers
+    answers.push(rightAnswer)
+    shuffleAnswers(answers)
+    searchAns(answers)
+}
+// shuffle answers
+function shuffleAnswers(answers) {
+    for (let i =0; i<answers.length; i=i+1) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [answers[i], answers[j]] = [answers[j], answers[i]];
 
-// // makeAnswers()
-//     // parse the data
-//     // transplate answer/shuffle logic here
-//     // append to DOM
+    }
+}
 
-// // checkAnswers()
-//     // if clicked answer is wrong
-//         // dont add a point => maybe add modal
-//         // make red border
-//         // or make invisible p tag that appears with write answer past into it
-//         // document.createElement(p).innerHTML(`data.rightanswer`)
-//     // if clicked answer is right
-//         // add a point => say congrats!
+function searchAns (answers){
+    console.log(answers)
+}
 
-// // timerFunc()
-//     // setTimeout(() => {
-//         // forloop subtracts 1 from 30
-//         // .innerHTML = i
-//     // }, 1000)
+// makeAnswers()
+    // parse the data
+    // transplate answer/shuffle logic here
+    // append to DOM
 
-// // easter egg?
-//     // bonus points!
+// checkAnswers()
+    // if clicked answer is wrong
+        // dont add a point => maybe add modal
+        // make red border
+        // or make invisible p tag that appears with write answer past into it
+        // document.createElement(p).innerHTML(`data.rightanswer`)
+    // if clicked answer is right
+        // add a point => say congrats!
+
+// timerFunc()
+    // setTimeout(() => {
+        // forloop subtracts 1 from 30
+        // .innerHTML = i
+    // }, 1000)
+
+// easter egg?
+    // bonus points!
 
